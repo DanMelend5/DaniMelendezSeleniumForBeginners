fc4c63f9d405768fe2c3fd65a192152713a22fe1
Daniel Melendez <danmelendez1986@gmail.com>
2025-06-16 23:25:46 -0300

    all test pass

--- src/test/java/com/practicetestautomation/tests/login/LoginTests.java
+++ src/test/java/com/practicetestautomation/tests/login/LoginTests.java
@@ -2,9 +2,9 @@ package com.practicetestautomation.tests.login;
 
 import com.practicetestautomation.pageobjects.LoginPage;
 import com.practicetestautomation.pageobjects.SuccessfulLoginPage;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -63,11 +63,13 @@ public class LoginTests {
         Assert.assertTrue(successfulLoginPage.isLogoutButtonDisplay());
     }
 
+
+    // Incorrect Username
     @Parameters({"invalidUsername", "validPassword", "expectedErrorMessage"})
     @Test(groups = {"negative", "regression"})
-    public void negativeLoginTest(String invalidUsername,
-                                  String validPassword,
-                                  String expectedErrorMessage) {
+    public void negativeUsernameLoginTest(String invalidUsername,
+                                          String validPassword,
+                                          String expectedErrorMessage) {
         logger.info("Starting testLoginFunctionality");
         LoginPage loginPage = new LoginPage(driver);
         loginPage.navigate();
@@ -76,13 +78,33 @@ public class LoginTests {
         loginPage.executeLogin(invalidUsername, validPassword);
         loginPage.getErrorMessage();
 
-        logger.info("Verify the expected error message: " + expectedErrorMessage);
         // Verify error message is displayed
-
+        logger.info("Verify the expected error message: " + expectedErrorMessage);
         Assert.assertTrue(loginPage.isErrorMessageDisplay());
 
         // Verify error message text is Your username is invalid!
+        Assert.assertEquals(loginPage.getErrorMessage(), expectedErrorMessage);
+    }
 
+    // Incorrect Password
+    @Parameters({"validUsername", "invalidPassword", "expectedErrorMessage"})
+    @Test(groups = {"negative", "regression"})
+    public void negativePasswordLoginTest(String validUsername,
+                                          String invalidPassword,
+                                          String expectedErrorMessage) {
+        logger.info("Starting testLoginFunctionality");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.navigate();
+        //executeLogin() returns successful login page. here we save the return value into a new variable
+        logger.info("Typing username: " + validUsername);
+        loginPage.executeLogin(validUsername, invalidPassword);
+        loginPage.getErrorMessage();
+
+        // Verify error message is displayed
+        logger.info("Verify the expected error message: " + expectedErrorMessage);
+        Assert.assertTrue(loginPage.isErrorMessageDisplay());
+
+        // Verify error message text is Your username is invalid!
         Assert.assertEquals(loginPage.getErrorMessage(), expectedErrorMessage);
     }
 }
